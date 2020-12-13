ddebf563c930072172b34bd28d48d9e4236f5998
JGreenOS <jsciteach@gmail.com>
2020-12-13 15:39:41 -0500

    working code to bring in lower case string from CharAt

--- Develop/script.js
+++ Develop/script.js
@@ -26,18 +26,56 @@ var uppercase = confirm("Do you want uppercase characters? Click Enter for Yes, 
 var spChar = confirm("Do you want special characters? Click Enter for Yes, Cancel for No")
 var number = confirm("Do you want numbers? Click Enter for Yes, Cancel for No")
 
-
-
-console.log(pwLength);
-console.log(lowercase);
-console.log(uppercase);
-console.log(spChar); 
-console.log(number);
-
-
+// Checks to make sure input is in the right format
+//console.log(pwLength);
+//console.log(lowercase);
+//console.log(uppercase);
+//console.log(spChar); 
+//console.log(number);
 
 
   //Step 2: select character from ASCII sets - lower, upper, numbers, and special characters
+
+  //Experiment: Using a function to make the array; I found this in researching "push" and building arrays and wanted to see if I could do it.
+ 
+  //function arrayLowToHigh(low, high) {
+   // const array = []
+   // for (let i = low; i <= high; i ++)
+   //  {
+   //   array.push(i)
+///}
+    //return array
+ // }
+
+    //var lowercasearray = [String.fromCharCode(65-90)];
+
+    var lowercasearray = [];
+    for (var i = 65; i < 91; i++)
+    {
+      lowercasearray.push(String.fromCharCode(i));
+    }
+    lowercase2 = lowercasearray;
+
+    console.log(lowercasearray);
+    
+
+
+
+
+
+
+
+ //function buildArrays (lower, upper, number, specialchar) {
+   // var lower = array.LOWER_CHAR_CODES(65, 90);
+    //var lower2 = str.CharCodeAt(str.lower);
+    //var numbers = String.fromCharCode (48, 57);
+    //var speChar = String.fromCharCode (58, 63; 33, 47);
+
+    //console.log(lower2);
+ 
+
+  
+
 
   //Step 3: Validate input, make sure at least 1 item is selected
 
@@ -46,5 +84,3 @@ console.log(number);
  // function generatePassword () {
 
   //Step 5: alert password
-
-    //}}
