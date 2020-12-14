2f2f84ee282f28564886c9538944aa34641ad23e
JGreenOS <jsciteach@gmail.com>
2020-12-14 14:07:34 -0500

    created variables with concatenation

--- Develop/script.js
+++ Develop/script.js
@@ -27,11 +27,11 @@ var spChar = confirm("Do you want special characters? Click Enter for Yes, Cance
 var number = confirm("Do you want numbers? Click Enter for Yes, Cancel for No")
 
 // Checks to make sure input is in the right format
-console.log(pwLength);
-console.log(lowercase);
-console.log(uppercase);
-console.log(spChar);
-console.log(number);
+//console.log(pwLength);
+//console.log(lowercase);
+//console.log(uppercase);
+//console.log(spChar);
+//console.log(number);
 
 
 //Step 2: select character from ASCII sets - lower, upper, numbers, and special characters
@@ -42,69 +42,81 @@ var lowercasearray = [];
 for (var i = 97; i < 123; i++) {
   lowercasearray.push(String.fromCharCode(i));
 }
-lowercase2 = lowercasearray;
-
-console.log(lowercasearray);
-
 
 var uppercasearray = [];
 for (var i = 65; i < 91; i++) {
   uppercasearray.push(String.fromCharCode(i));
 }
-uppercase2 = uppercasearray;
-
-console.log(uppercasearray);
 
 var numberarray = [];
 for (var i = 48; i < 58; i++) {
   numberarray.push(String.fromCharCode(i));
 }
-numberarray2 = numberarray;
-
-console.log(numberarray);
 
 var symbolarray = [];
 for (var i = 33; i < 48; i++) {
   symbolarray.push(String.fromCharCode(i));
 }
-symbolarray2 = symbolarray;
-
-console.log(symbolarray);
 
 //Success after some deep digging in w3schools fromCharCode and a few YouTube samples!  THere is a way to make this cleaner and DRY, but I won't work on this unless I have time.
 
 
 //Step 3: Validate input, make sure at least 1 item is selected
 
-if (lowercase) "0" {
- && (uppercase) "0";
+
+
+{
+
+//alert("Please start over. You must select at least 1 condition (lower case, upper case, special character, or number)");
 }
 
-//spChar false,
-//number false,
-
-alert("Please start over. You must select at least 1 condition (lower case, upper case, special character, or number)");
-}
-  alert("Password complete");
-}
 
 //Step 4: Create password using input requirements  (Look for inputs to be true, concatenate the strings, then choose random number and fill the password until the variable pwLength is satisfied. 
-var pw2;
-var temporarypw;
 
-function generatePassword(pwLength, lowercase, uppercase, spChar, number) {
-  if (uppercase) true; charCodes.concat(uppercase2);
-  if (lowercase) true; charCodes.concat(lowercase2);
-  if (spChar) true; charCodes.concat(symbolarray2);
-  if (number) true; charCodes.concat(numberarray2);
 
-  console.log(charCodes);
+var nuls = lowercasearray.concat(uppercasearray, numberarray, symbolarray);
 
-  for (let i = 0; i < pwLength; i++); {
-    temporarypw = charCodes[Math.floor(Math.random() * pwLength)]
+var nul = lowercasearray.concat(uppercasearray, numberarray);
+
+var ul = lowercasearray.concat(uppercasearray);
+
+var sul = lowercasearray.concat(uppercasearray, symbolarray);
+
+var us = uppercasearray.concat(symbolarray);
+
+var un = numberarray.concat(uppercasearray);
+
+var nus = numberarray.concat(uppercasearray, symbolarray);
+
+var ls = lowercasearray.concat(symbolarray);
+
+var ln = lowercasearray.concat(numberarray);
+
+var sn = symbolarray.concat(numberarray);
+
+
+
+//console.log(nuls);
+//console.log(nul);
+//console.log(ul);
+//console.log(sul);
+//console.log(us);
+//console.log(un);
+//console.log(nus);
+//console.log(ls);
+//console.log(ln);
+//console.log(sn);
+
+
+//function generatePassword(pwLength, lowercase, uppercase, spChar, number) {
+ //if (
+
+  
+
+  //for (let i = 0; i < pwLength; i++); {
+    //temporarypw = charCodes[Math.floor(Math.random() * pwLength)]
 
 
     //Step 5: alert password
 
-  }
-}
+  
