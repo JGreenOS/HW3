32f69be4ac77526ed04a6074ac4327cb5ef523dd
JGreenOS <jsciteach@gmail.com>
2020-12-16 20:12:37 -0500

    pw created, not displaying in HTML element

--- Develop/script.js
+++ Develop/script.js
@@ -3,13 +3,11 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+var password = generatePassword();
+var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
+passwordText.value = password;
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
@@ -17,21 +15,18 @@ generateBtn.addEventListener("click", writePassword);
 
 var pwLength = Number(prompt("Enter the number of characters for your new password. Minimum is 8 characters and maximum is 128 characters"));
 
-//Step 3: Validate input using while loop; fixed logic error that sent this into infinity.
+//Step 2: Validate input using while loop; fixed logic error that sent this into infinity.
 
 while (pwLength < 8 || pwLength > 128) {
   alert("Password must be between 8-128 characters. Try again.");
   var pwLength = Number(prompt("Enter the number of characters for your new password. Minimum is 8 characters and maximum is 128 characters"));
-
 }
-
 var lowercase = confirm("Do you want lowercase characters? Click OK for Yes, Cancel for No")
 var uppercase = confirm("Do you want uppercase characters? Click OK for Yes, Cancel for No")
 var spChar = confirm("Do you want special characters? Click OK for Yes, Cancel for No")
 var number = confirm("Do you want numbers? Click OK for Yes, Cancel for No")
 
-
-//Step 2: select character from ASCII sets - lower, upper, numbers, and special characters
+//Step 3: select character from ASCII sets - lower, upper, numbers, and special characters
 
 //Experiment: Using a function to make the array; I found this in researching "push" when building arrays and wanted to see if I could do it.
 
@@ -56,45 +51,48 @@ for (var i = 33; i < 48; i++) {
 }
 //Success after some deep digging in w3schools fromCharCode and a few YouTube samples!  THere is a way to make this cleaner and DRY, but I won't work on this unless I have time.
 
-//Step 3: Create password using input requirements  (Look for inputs to be true, concatenate the strings, then choose random number and fill the password until the variable pwLength is satisfied. 
+//Step 4: Create password using input requirements  (Look for inputs to be true, concatenate the strings, then choose random number and fill the password until the variable pwLength is satisfied)
+var allChars
 
 function generatePassword() {
   if (lowercase && uppercase && spChar && number)
     allChars = lowercasearray.concat(uppercasearray, numberarray, symbolarray);
 
   else if (lowercase && uppercase && spChar)
-  allChars = lowercasearray.concat(uppercasearray, spChar);
+    allChars = lowercasearray.concat(uppercasearray, spChar);
 
   else if (lowercase && uppercase)
-  allChars = lowercasearray.concat(uppercasearray)
+    allChars = lowercasearray.concat(uppercasearray)
 
   else if (uppercase && spChar && number)
-  allChars = uppercasearray.concat(spChararray, numberarray);
+    allChars = uppercasearray.concat(spChararray, numberarray);
 
   else if (spChar && number)
-  allChars = spChararray.concat(numberarray);
+    allChars = spChararray.concat(numberarray);
 
-
-  //for (let i = 0; i < pwLength; i++); {
-  //password[Math.floor(Math.random() * pwLength.length)];}
+ 
 
   for (let i = 0; i < pwLength; i++) {
     number = Math.random() * allChars.length;
     Math.floor(number);
-  }
+    var writePassword = writePassword + number;
+
+  
+
+  console.log(allChars);
+  //console.log(randomPassword);
+
   return password;
-    
-    //red = allChars(number);  
 
-    //console.log(number);
-    //console.log(red);
-    //console.log(allChars);
-    
   }
-  //randomisze the lowercase.array(the whole thing)
  
+  //red = allChars(number);  
 
+  //console.log(number);
+  //console.log(red);
+  //console.log(allChars);
 
-
-
-
+  //randomisze the lowercase.array(the whole thing)
+ //for (let i = 0; i < pwLength; i++); {
+  //password[Math.floor(Math.random() * pwLength.length)];
+}
