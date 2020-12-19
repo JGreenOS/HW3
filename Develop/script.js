d9f064040d0dafe5a70432fc530faeab08b16136
JGreenOS <jsciteach@gmail.com>
2020-12-18 19:44:41 -0500

    Reworked entire file logic from code review with TA

--- Develop/script.js
+++ Develop/script.js
@@ -1,35 +1,28 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-var password = generatePassword();
 var passwordText = document.querySelector("#password");
 
-passwordText.value = password;
-}
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
 
-//Step 1: prompt for length of password, set pwLength variable and prompt for number of characters in pw between 8 and 128.  Change input from string to number.
+//This area for user input
+Text.value = password;
+
+generateBtn.addEventListener("click", writePassword);
 
 var pwLength = Number(prompt("Enter the number of characters for your new password. Minimum is 8 characters and maximum is 128 characters"));
 
-//Step 2: Validate input using while loop; fixed logic error that sent this into infinity.
-
+//keep this loop
 while (pwLength < 8 || pwLength > 128) {
   alert("Password must be between 8-128 characters. Try again.");
   var pwLength = Number(prompt("Enter the number of characters for your new password. Minimum is 8 characters and maximum is 128 characters"));
 }
+
+//Experiment: Using a function to make the array; I found this in researching "push" when building arrays and wanted to see if I could do it.
+
+//move this to inside the get characters function to tie them together and create the overall function to create strings
 var lowercase = confirm("Do you want lowercase characters? Click OK for Yes, Cancel for No")
 var uppercase = confirm("Do you want uppercase characters? Click OK for Yes, Cancel for No")
 var spChar = confirm("Do you want special characters? Click OK for Yes, Cancel for No")
 var number = confirm("Do you want numbers? Click OK for Yes, Cancel for No")
-
-//Step 3: select character from ASCII sets - lower, upper, numbers, and special characters
-
-//Experiment: Using a function to make the array; I found this in researching "push" when building arrays and wanted to see if I could do it.
-
 var lowercasearray = [];
 for (var i = 97; i < 123; i++) {
   lowercasearray.push(String.fromCharCode(i));
@@ -51,48 +44,60 @@ for (var i = 33; i < 48; i++) {
 }
 //Success after some deep digging in w3schools fromCharCode and a few YouTube samples!  THere is a way to make this cleaner and DRY, but I won't work on this unless I have time.
 
-//Step 4: Create password using input requirements  (Look for inputs to be true, concatenate the strings, then choose random number and fill the password until the variable pwLength is satisfied)
-var allChars
+
+//Event listener goes here, generate password function in this area and math to randomize.
+
+function writePassword() {
+  var password = generatePassword();
+  
+
+  password
 
 function generatePassword() {
+  var allChars = [];
   if (lowercase && uppercase && spChar && number)
     allChars = lowercasearray.concat(uppercasearray, numberarray, symbolarray);
 
-  else if (lowercase && uppercase && spChar)
+  else if (lowercase && uppercase && spChar){
     allChars = lowercasearray.concat(uppercasearray, spChar);
-
-  else if (lowercase && uppercase)
+    //console.log(allChars);
+  }
+  else if (lowercase && uppercase){
     allChars = lowercasearray.concat(uppercasearray)
-
-  else if (uppercase && spChar && number)
+    //console.log(allChars);
+  }
+  else if (uppercase && spChar && number){
     allChars = uppercasearray.concat(spChararray, numberarray);
-
-  else if (spChar && number)
+    //console.log(allChars);}
+  }
+  else if (spChar && number){
     allChars = spChararray.concat(numberarray);
-
- 
+    //console.log(allChars);}
+  }
+var purple;
 
   for (let i = 0; i < pwLength; i++) {
-    number = Math.random() * allChars.length;
-    Math.floor(number);
-    var writePassword = writePassword + number;
+    var diffName = Math.random() * allChars.length;
+    diffname = Math.floor(diffName);
+    
+    console.log(diffname);
+    console.log(allChars[diffName]);
+    console.log(allChars.length);
+    console.log(typeof allChars);
 
   
+return;
+}
+}
 
-  console.log(allChars);
-  //console.log(randomPassword);
 
-  return password;
-
-  }
- 
   //red = allChars(number);  
-
+//var writePassword = writePassword + number;
   //console.log(number);
   //console.log(red);
   //console.log(allChars);
 
   //randomisze the lowercase.array(the whole thing)
  //for (let i = 0; i < pwLength; i++); {
-  //password[Math.floor(Math.random() * pwLength.length)];
+  //password[Math.floor(Math.random() * pwLength.length)]
 }
---
+++ solution.js
@@ -0,0 +1,73 @@
+//solution
+var generateBtn = document.querySelector("#generate");
+var passwordTxt = document.querySelector("#password");
+var usrInputObject = {
+  passLength: 0,
+  charList: [],
+  getPassLength: function () {
+    var pwLength = Number(prompt("Enter the number of characters for your new password. Minimum is 8 characters and maximum is 128 characters"));
+    while (pwLength < 8 || pwLength > 128) {
+      alert("Password must be between 8-128 characters. Try again.");
+      pwLength = Number(prompt("Enter the number of characters for your new password. Minimum is 8 characters and maximum is 128 characters"));
+    }
+    this.passLength = pwLength;
+    return this;
+  },
+  getAllChars: function () {
+    var lowercase = confirm("Do you want lowercase characters? Click OK for Yes, Cancel for No")
+    var uppercase = confirm("Do you want uppercase characters? Click OK for Yes, Cancel for No")
+    var spChar = confirm("Do you want special characters? Click OK for Yes, Cancel for No")
+    var number = confirm("Do you want numbers? Click OK for Yes, Cancel for No")
+    var lowercasearray = [];
+    for (var i = 97; i < 123; i++) {
+      lowercasearray.push(String.fromCharCode(i));
+    }
+    var uppercasearray = [];
+    for (var i = 65; i < 91; i++) {
+      uppercasearray.push(String.fromCharCode(i));
+    }
+    var numberarray = [];
+    for (var i = 48; i < 58; i++) {
+      numberarray.push(String.fromCharCode(i));
+    }
+    var symbolarray = [];
+    for (var i = 33; i < 48; i++) {
+      symbolarray.push(String.fromCharCode(i));
+    }
+    var allChars;
+    if (lowercase && uppercase && spChar && number) {
+      allChars = lowercasearray.concat(uppercasearray, numberarray, symbolarray);
+    } else if (lowercase && uppercase && spChar) {
+      allChars = lowercasearray.concat(uppercasearray, spChar);
+    } else if (lowercase && uppercase) {
+      allChars = lowercasearray.concat(uppercasearray)
+    } else if (uppercase && spChar && number) {
+      allChars = uppercasearray.concat(spChararray, numberarray);
+    } else if (spChar && number) {
+      allChars = spChararray.concat(numberarray);
+    }
+    this.charList = allChars;
+    return this;
+  }
+}
+generateBtn.addEventListener("click", function () {
+  passwordTxt.value = generatePassword();
+});
+function generatePassword() {
+  var password = "";
+  var input = usrInputObject.getPassLength().getAllChars();
+  var pwLength = input.passLength;
+  var listLength = input.charList.length;
+  console.log("password length..." + pwLength);
+  console.log("char list length..." + listLength);
+  console.log(input.charList);
+  for (let i = 0; i < pwLength; i++) {
+    var raw = Math.random() * listLength - 1;
+    var randomIndex = Math.floor(raw);
+    password += input.charList[randomIndex];
+    console.log(raw);
+    console.log("random index for a char in the char list: " + randomIndex);
+    console.log("random character choice: " + input.charList[randomIndex]);
+    console.log("password at the current iteration: " + password);
+  }
+  return password;
